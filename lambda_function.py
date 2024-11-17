a8548972381ec87f5a90f0600c3948d2e465cd5c
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-11-17 15:19:15 -0500

    added 3rd addtion

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,8 @@ import json
 def lambda_handler(event, context):
     num1 = int(event.get('num1', 0))
     num2 = int(event.get('num2', 0))
-    result = num1 + num2
+    num3 = int(event.get('num2', 0))
+    result = num1 + num2 + num3
     
 
     return {
