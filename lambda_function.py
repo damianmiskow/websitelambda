ee2c0aa4de1399cb278b303251054177fc8beaad
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-11-17 15:27:02 -0500

    improved calc

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,12 @@
 import json
 
 def lambda_handler(event, context):
+    op = event.get('operator')
     num1 = int(event.get('num1', 0))
     num2 = int(event.get('num2', 0))
-    num3 = int(event.get('num2', 0))
-    result = num1 + num2 + num3
+
+    if op == "*":
+        result = num1 * num2
     
 
     return {
